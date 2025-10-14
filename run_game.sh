80a7f6920f4b2f2a45e1f75f8a3f8254b981e4bc
jchen2090 <jasonchen5044@gmail.com>
2025-10-14 10:58:29 -0400

    update run script

--- run_game.sh
+++ run_game.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-cd client
+trap on_interrupt SIGINT SIGTERM
+
+on_interrupt() {
+    echo "Ctrl + C detecting, shutting down server and client" 
+
+    server_pid=$(lsof -t -i:3000)
+    client_pid=$(lsof -t -i:5173)
+
+    if [[ -n "$server_pid" ]]; then 
+        echo "Killing server process" 
+        kill "$server_pid"
+    fi
+
+    if [[ -n "$client_pid" ]]; then 
+        echo "Killing client process" 
+        kill "$client_pid"
+    fi
+
+    exit 0
+}
+
+
+cd client || exit
 npm i 
 npm run dev & 
-cd ../server 
+cd ../server  || exit
 npm i 
-npm run dev
+npm run dev & 
+firefox --kiosk http://localhost:5173 &
+
+wait
